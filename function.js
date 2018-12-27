5a2f32544e16b023ea677109ad842545a6f72232
Sebastian Tramper <tramper.s@outlook.com>
2018-12-27 14:25:55 +0100

    saved my current version of this project on github

---
+++ function.js
@@ -0,0 +1,9 @@
+$('.nav-toggle').click(function() {
+    $('.side-nav').toggleClass('toggled');
+    $('.body').toggleClass('body-fixed');
+});
+
+$('.nav-close').click(function(){
+    $('.side-nav').removeClass('toggled')
+    $('.body').removeClass('body-fixed');
+})
---
+++ index.php
@@ -0,0 +1,131 @@
+<html lang="nl">
+<head>
+    <meta charset="UTF-8">
+    <!-- legacy html5 support voor IE -->
+    <!--[if lt IE 9]>
+    <script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
+    <![endif]-->
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="css/normalize.css">
+    <link rel="stylesheet" href="css/grid-layout.css">
+    <link rel="stylesheet" href="css/styles.css">
+    <link rel="stylesheet" href="css/media.css">
+    <!-- change the title -->
+    <title>Chanel & Daisy Kapsters aan huis</title>
+</head>
+<!--body = main grid -->
+<body class="body">
+    <!-- Header section -->
+    <header>
+        <!-- navigation section -->
+        <img src="img/menu.svg" alt="menu " class="nav-toggle" >
+        <section class="side-nav">
+            <img src="img/close.svg" alt="close" class="nav-close">
+            <nav>
+                <ul>
+                    <li class="nav-1"><a href="contact.php">Afspraak maken</a></li>
+                    <li class="nav-2"><a href="index.php">Homepagina</a></li>
+                    <li class="nav-3"><a href="producten.html">Producten</a></li>
+                    <li class="nav-4"><a href="diensten.html">Diensten</a></li>
+                </ul>
+            </nav>
+        </section>
+        <section class="header-mid">
+            <div class="header-title"> 
+                <h1 class="header-h1">Chanel & Daisy<br><span>Kapsters  aan huis</span></h1>
+            </div>  
+        </section>
+        <slider>
+            <slider><p></p></slider>
+            <slider><p></p></slider>
+            <slider><p></p></slider>
+            <slider><p></p></slider>
+        </slider>
+        <div class="header-regio-background"><h2 class="header-regio">Regio Reimerswaal & Walcheren</h2></div>
+    </header>
+    <!--main section -->
+    <main>
+        <!-- Over ons section -->
+        <article class=" main-1 main-article">
+            <h2>Over ons</h2>
+            <img src="img/profiel.png" alt="profiel" class="header-profiel">
+            <div><p>Hallo, Wij zijn Chanel en Daisy, twee zussen en kapsters aan huis. Wilt u een betrouwbare, gezellige en hygiënische thuiskapster? Dan bent u bij ons aan het goede adres! Wij zijn voorzien van een mobiele wastafel, dat maakt een behandeling bij u thuis nog veel makkelijker!
+Wij knippen, kleuren, permanenten, föhnen en u kunt ons ook bellen voor een opsteekkapsel. Ook doen wij kinderen knippen en zijn wij altijd bereid advies te geven over uw kapsel. Interesse? Bel gerust! Voor omgeving Krabbendijke belt u Chanel: 0612779366 Voor omgeving s’Heerenhoek belt u Daisy: 0612611293
+Als de één niet kan valt de ander in. U hoeft dus nooit lang te wachten op een afspraak.
+</p></div>
+        </article>
+        <!-- Diensten section -->
+        <section class="main-2">
+            <h2>Diensten</h2>
+            <section class="main-diensten"><figure class="main-figure-1">
+                    <img src="https://source.unsplash.com/user/erondu/2100x1000" alt="dienst">
+                    <figcaption>Hier komt info over de dienst</figcaption>
+                </figure>
+                <figure>
+                    <img src="https://source.unsplash.com/user/erondu/2100x1000" alt="dienst">
+                    <figcaption>Hier komt info over de dienst</figcaption>
+                </figure>
+                <figure>
+                    <img src="https://source.unsplash.com/user/erondu/2100x1000" alt="dienst">
+                    <figcaption>Hier komt info over de dienst</figcaption>
+                </figure>
+                <figure>
+                    <img src="https://source.unsplash.com/user/erondu/2100x1000" alt="dienst">
+                    <figcaption>Hier komt info over de dienst</figcaption>
+                </figure>
+                <figure>
+                    <img src="https://source.unsplash.com/user/erondu/2100x1000" alt="dienst">
+                    <figcaption>Hier komt info over de dienst</figcaption>
+                </figure>
+            </section>
+        </section>
+        <section class="main-3" role="advertentie">
+            <!-- hier komen advertentie's -->
+        </section>
+        <!-- opitoneel producten -->
+        <section class="main-4">
+            <h2>Producten</h2>
+            <figure>
+                <img src="https://source.unsplash.com/user/erondu/2100x1000" alt="dienst">
+                <figcaption>Hier komt een product</figcaption>
+            </figure>
+            <figure>
+                <img src="https://source.unsplash.com/user/erondu/2100x1000" alt="dienst">
+                <figcaption>Hier komt een product</figcaption>
+            </figure>
+            <figure>
+                <img src="https://source.unsplash.com/user/erondu/2100x1000" alt="dienst">
+                <figcaption>Hier komt een product</figcaption>
+            </figure>
+            <figure>
+                <img src="https://source.unsplash.com/user/erondu/2100x1000" alt="dienst">
+                <figcaption>Hier komt een product</figcaption>
+            </figure>
+            <figure>
+                <img src="https://source.unsplash.com/user/erondu/2100x1000" alt="dienst">
+                <figcaption>Hier komt een product</figcaption>
+            </figure>
+        </section>
+    </main>
+    <!-- footer section -->
+    <footer class="footer-main">
+        <section class="footer-upper-section">
+            <address>
+                <p class="">Volg ons op social media</p>
+                <a href="https://www.facebook.com/"><img src="img/footer-facebook.png" alt="Facebook-icon" class="footer-icon"></a>
+                <a href="https://www.instagram.com/?hl=en"><img src="img/footer-instagram.png" alt="Instagram-icon" class="footer-icon"></a>
+                <a href="https://www.linkedin.com/"><img src="img/footer-linkedin.png" alt="Linkedin-icon" class="footer-icon"></a>
+            </address>
+            <small><p>Copyright &copy; Chanel & Daisy</p></small>
+        </section>
+        <section class="footer-lower-section">
+            <h6>Website gemaakt door:</h6>
+            <a href="http://iamsebastiantramper.com/" rel="author"><h6>Sebastian Tramper</h6></a>
+            <address><a href="http://iamsebastiantramper.com/" rel="author"><p>www.iamsebastiantramper.com</p></a></address>
+        </section>
+    </footer>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js" defer></script>
+    <script type="text/javascript" src="function.js" defer></script>
+</body>
+</html>
